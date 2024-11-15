0fd9a31ac3b3a0ea3fd5bc17552cc742cc8dd11f
Bobejoo <ej0b0bej0@gmail.com>
2024-11-15 19:11:10 +0000

    groovy jobs 4 front,back and app of apps repos added

---
+++ jenkins_controller/jobs/app_of_apps.groovy
@@ -0,0 +1,21 @@
+pipelineJob('app_of_apps') {
+    parameters {
+        stringParam('backendDockerTag', 'latest', 'Backend docker image tag')
+        stringParam('frontendDockerTag', 'latest', 'Frontend docker image tag')
+    }
+ 
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        credentials("")
+                        url("https://github.com/Bobejoo/App_of_apps.git")
+                    }
+                    branches('main')
+                    scriptPath('Jenkinsfile')
+                }
+            }
+        }
+    }
+}
